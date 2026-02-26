985dd926a095d1e11414cd46c27a769778020141
mo-xi-nb <2435083771@qq.com>
2026-02-26 09:37:30 +0800

    Delete src directory

--- src/bolt/listeners/messages/index.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from "@slack/bolt";
-import { sampleMessageCallback } from "./sample-message.js";
-
-const register = (app: App) => {
-  app.message(/^hello.*/, sampleMessageCallback);
-};
-
-export default { register };
